6a123a8775c8970d7f73ee589727e76f7ba1e0c4
Timkov25 <timkov25@gmail.com>
2020-02-05 13:59:13 +0300

    Добавлен парсер скобок
    Парсер скобок добавлен в основную прошивку в качестве функции, работающей по флагу

--- Search_breaket_and_String_to_float/Search_breaket_and_String_to_float.ino
+++ Search_breaket_and_String_to_float/Search_breaket_and_String_to_float.ino
@@ -1,9 +1,9 @@
-#define value 10   //Максимальное значение ответов в скобках
+#define value 10   //Максимальное значение ответов в массивах парсера
 //----Тестовая строка----
-String string0 = String("Ща_будут_данные(123.456)и_еще_данные(34.234)а_сейчас_не_будет(544.456)Ха_ха_обманул(123.12)два_манула(654.321)три_манула");
-String Ans_string[value] = {}; //Массив с ответами в string
+String AnswerString = String("Ща_будут_данные(123.456)и_еще_данные(34.234)а_сейчас_не_будет(544.456)Ха_ха_обманул(123.12)два_манула(654.321)три_манула");
 String buf;                     //Буферная строка для преобразования во float
 float Ans_float[value] = {};    //Массив с ответами во float
+String Ans_string[value] = {}; //Массив с ответами в string
 int left_breaket[value] = {};   //Массив с позициями '('
 int right_breaket[value] = {};  //Массив с позициями ')'
 int left_breaket_counter = 0;  //Счетчик левых 
@@ -14,7 +14,7 @@ void setup() {
 
 
 //----Ищем позиции скобок и сохряняем их в массивы
-  for (int i = 0; i < string0.length(); i++) {
+  for (int i = 0; i < AnswerString.length(); i++) {
     if (string0[i] == '(') {
       left_breaket[left_breaket_counter] = i;
       left_breaket_counter++;
@@ -26,21 +26,19 @@ void setup() {
   }
 //----Сохраняем данные из скобок в массивы с ответами----
   for (int i = 0; i < left_breaket_counter; i++) {
-    Ans_string[i] = string0.substring(left_breaket[i] + 1, right_breaket[i]);
-    buf = string0.substring(left_breaket[i] + 1, right_breaket[i]);
+    Ans_string[i] = AnswerString.substring(left_breaket[i] + 1, right_breaket[i]);
+    buf = AnswerString.substring(left_breaket[i] + 1, right_breaket[i]);
     Ans_float[i] = buf.toFloat();
   }
 
-
-//---- Выводим ответы в float ----
-  for (int i = 0; i < left_breaket_counter ; i++) {
-    Serial.print("Ans: ");
-    Serial.println(Ans_float[i]);
-  }
+////---- Выводим ответы во float ----
+//  for (int i = 0; i < left_breaket_counter ; i++) {
+//    Serial.print("Ans: ");
+//    Serial.println(Ans_float[i]);
+//  }
 //----Обнуляем счетчики скобок----
   left_breaket_counter = 0;
   right_breaket_counter = 0;
-
 }
 void loop() {
   // put your main code here, to run repeatedly:
--- group_request_8N1/group_request_8N1.ino
+++ group_request_8N1/group_request_8N1.ino
@@ -7,7 +7,6 @@ GTimer requestTimer(MS); //Таймер запросов
 GTimer stringCollectTimer(MS);  //Таймаут сбора строки после запроса
 
 
-
 int switch_case_val = 0;   //Переменная для запроса
 bool  flag_string_collect = 0; //флаг сбора строки
 bool flag_breaket_parse = 0; //флаг парсинга скобок
@@ -20,6 +19,16 @@ bool flag_breaket_parse = 0; //флаг парсинга скобок
 
 String AnswerString;  //Строка для сбора ответа
 
+//---------- Данные для парсера ----------
+String buf;                     //Буферная строка для преобразования во float
+float Ans_float[value] = {};    //Массив с ответами во float
+String Ans_string[value] = {}; //Массив с ответами в string
+int left_breaket[value] = {};   //Массив с позициями '('
+int right_breaket[value] = {};  //Массив с позициями ')'
+int left_breaket_counter = 0;  //Счетчик левых
+int right_breaket_counter = 0; //              и правых скобок в строке
+
+
 // открываем сессию групового опроса
 byte init_message[] = {0x2f, 0x3f, 0x21, 0x0d, 0x0a}; //   /?!<CR><LF>
 // задаем режим считывания
@@ -28,32 +37,33 @@ byte mode_message[] = {0x06,  0x30,  0x35,  0x31,  0x0d, 0x0a};   // <ACK>VZY<CR
 byte data_message[] = {0x01, 0x52,  0x31,  0x02,  0x45, 0x54, 0x30, 0x50, 0x45, 0x28, 0x29,  0x03,  0x37};
 
 //----- пин управления прием/передача для модуля ttl-rs485 -----
-#define RS485_PIN 8 
+#define RS485_PIN 8
 
 /*
- * --------Строка для использования software serial---------------
+   --------Строка для использования software serial---------------
   SoftwareSerial RS485 (7, 6); // RX, TX
 */
 void setup() {
-//------------Инициализация Serial------------
+  //------------Инициализация Serial------------
   Serial.begin(9600, SERIAL_7E1);
 
-//---------Инициализация Таймера для запросов--------------------
+  //---------Инициализация Таймера для запросов--------------------
   requestTimer.setInterval(1000);
 
 
-//----------Инициализация пина для модуля ttl-RS-485----------------
-//Вторая строка, возможно, не нужна
+  //----------Инициализация пина для модуля ttl-RS-485----------------
+  //Вторая строка, возможно, не нужна
   pinMode(RS485_PIN, OUTPUT);
   digitalWrite(RS485_PIN, HIGH);
 }
 void loop() {
-  
+
   request();
 
   string_collect();
 
-  
+  breaket_parser();
+
 
 }
 /*   Тот же switch case только через if (с форума)
@@ -119,25 +129,55 @@ void request() {
 char read_bufer () {
   if (Serial.available()) {
     char response = Serial.read();
-/*    Если передаваемый при Serial.begin параметр о паритете 7E1     
- *     не заработает, то эта строка может заработать как кастыльная 
- *     альтернатива
+    /*    Если передаваемый при Serial.begin параметр о паритете 7E1
+           не заработает, то эта строка может заработать как кастыльная
+           альтернатива
 
-        response &= 0x7F;// convert 8N1 to 7E1
-*/        
+            response &= 0x7F;// convert 8N1 to 7E1
+    */
     return response;
   }
 }
 //---------- Функция сбора принимаемых символов в строку ----------
 void string_collect() {
-//  Собираем строку пока поднят флаг
+  //  Собираем строку пока поднят флаг
   if (flag_string_collect) {
     AnswerString += read_bufer();
   }
-//  Опускаем флаг когда тайаут заканчивается
+  //  Опускаем флаг когда тайаут заканчивается
   if (stringCollectTimer.isReady()) {
     flag_string_collect = false;
     flag_breaket_parse = true;
-    
+
+  }
+}
+
+void breaket_parser() {
+  if (flag_breaket_parse) {
+    //----Ищем позиции скобок и сохряняем их в массивы
+    for (int i = 0; i < AnswerString.length(); i++) {
+      if (string0[i] == '(') {
+        left_breaket[left_breaket_counter] = i;
+        left_breaket_counter++;
+      }
+      if (string0[i] == ')') {
+        right_breaket[right_breaket_counter] = i;
+        right_breaket_counter++;
+      }
+    }
+    //----Сохраняем данные из скобок в массивы с ответами----
+    for (int i = 0; i < left_breaket_counter; i++) {
+      Ans_string[i] = AnswerString.substring(left_breaket[i] + 1, right_breaket[i]);
+      buf = AnswerString.substring(left_breaket[i] + 1, right_breaket[i]);
+      Ans_float[i] = buf.toFloat();
+    }
+
+
+    //----Обнуляем счетчики скобок----
+    left_breaket_counter = 0;
+    right_breaket_counter = 0;
+
+    //-----Опускаем флаг парсера-----
+    flag_breaket_parse = false;
   }
 }
